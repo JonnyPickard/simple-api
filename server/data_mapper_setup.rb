327c0d2ca9538f7fbb5f017bfa2e1de41edb9289
Jonny Pickard <jonathandpickard@gmail.com>
2016-05-29 22:28:50 +0100

    Production version running on heroku

--- server/data_mapper_setup.rb
+++ server/data_mapper_setup.rb
@@ -2,7 +2,10 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative 'models/user'
 
-DataMapper.setup(:default, 'postgres://localhost/sinatra_api_test')
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/sinatra_api_test")
+
+
 DataMapper.finalize
 # DataMapper.auto_migrate!
 DataMapper.auto_upgrade!
--- user-interface/controllers/user_controller.rb
+++ user-interface/controllers/user_controller.rb
@@ -3,19 +3,14 @@ require 'httparty'
 require 'json'
 
 class UserController < Sinatra::Base
-  set :port, 9494
   set :root, File.join(File.dirname(__FILE__), '..')
-
-  before do
-    p params
-  end
 
   get "/" do
     redirect to"/users"
   end
 
   get "/users" do
-    response = HTTParty.get("http://localhost:4567/users")
+    response = HTTParty.get("http://jonny-simple-api-server.herokuapp.com/users")
     @users = JSON.parse(response)
     erb :index, :layout => :'templates/layout'
   end
@@ -25,29 +20,29 @@ class UserController < Sinatra::Base
   end
 
   post "/users/new" do
-    HTTParty.post("http://localhost:4567/users/new", :query => params )
+    HTTParty.post("http://jonny-simple-api-server.herokuapp.com/users/new", :query => params )
     redirect to"users"
   end
 
   get "/users/edit" do
-    response = HTTParty.get("http://localhost:4567/users")
+    response = HTTParty.get("http://jonny-simple-api-server.herokuapp.com/users")
     @users = JSON.parse(response)
     erb :"users/edit", :layout => :'templates/layout'
   end
 
   post "/users/edit" do
-    HTTParty.put("http://localhost:4567/users/#{params["user_id"]}", :query => params )
+    HTTParty.put("http://jonny-simple-api-server.herokuapp.com/users/#{params["user_id"]}", :query => params )
     redirect to"users/edit"
   end
 
   get "/users/delete" do
-    response = HTTParty.get("http://localhost:4567/users")
+    response = HTTParty.get("http://jonny-simple-api-server.herokuapp.com/users")
     @users = JSON.parse(response)
     erb :"users/delete", :layout => :'templates/layout'
   end
 
   post "/users/delete" do
-    HTTParty.post("http://localhost:4567/users/#{params["user_id"]}", :query => params )
+    HTTParty.post("http://jonny-simple-api-server.herokuapp.com/users/#{params["user_id"]}", :query => params )
     redirect to"users/delete"
   end
 
